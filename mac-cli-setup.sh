02a472251c5c95db0f75afc1ad074c7d06a4560e
Evan Vance <72946268+evandvance@users.noreply.github.com>
2025-05-27 08:29:42 -0500

    Troubleshoot script

--- mac-cli-setup.sh
+++ mac-cli-setup.sh
@@ -2,7 +2,8 @@
 
 # This script checks if Geneious Prime is installed and creates a symbolic link to the CLI tool for macOS
 
-if [[ -d /Applications/Geneious\ Prime.app/Contents/Resources/app/resources/geneious ]]; then
+if [[ -d /Applications/Geneious\ Prime.app ]] ; then
+    echo "Creating symbolic link for Geneious CLI tool..."
     sudo ln -sf /Applications/Geneious\ Prime.app/Contents/Resources/app/resources/geneious /usr/local/bin
 else
     echo "Geneious Prime not found"
