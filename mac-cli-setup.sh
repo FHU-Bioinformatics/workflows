329fc751f02b05125ff5fa60051628d9d302541b
Evan Vance <72946268+evandvance@users.noreply.github.com>
2025-05-27 08:37:32 -0500

    Add breakout clause for non-mac

--- mac-cli-setup.sh
+++ mac-cli-setup.sh
@@ -2,6 +2,11 @@
 
 # This script checks if Geneious Prime is installed and creates a symbolic link to the CLI tool for macOS
 
+if [[ $(uname) != "Darwin" ]]; then
+    echo "This script is only for macOS"
+    exit 1
+fi
+
 if [[ -d /Applications/Geneious\ Prime.app ]] ; then
     echo "Creating symbolic link for Geneious CLI tool..."
     sudo ln -sf /Applications/Geneious\ Prime.app/Contents/Resources/app/resources/geneious /usr/local/bin
