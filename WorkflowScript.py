ce561313c5cb5cee608a5960c96e3271f06cebe2
thabomahlaha <rogermahlaha@outlook.com>
2025-06-09 13:06:32 -0500

    Creating two-step glob structure

--- WorkflowScript.py
+++ WorkflowScript.py
@@ -2,27 +2,27 @@ import argparse
 from pathlib import Path
 import subprocess
 
+
 def run_workflow(input_path, workflow_path, output_path):
     input_path = Path(input_path)
     workflow_path = Path(workflow_path)
     output_path = Path(output_path)
+    output_path.mkdir(parents=True, exist_ok=True)
 
-    for barcode_directory in input_path.iterdir():
-        if barcode_directory.is_dir():
-            fastq_files = list(barcode_directory.glob("*.fastq.gz"))
+    #The idea is to have a two-step glob where you get the directories in a subdirectory, then the files in those directories.
+    subdirectories = [barcode_directory for barcode_directory in input_path.iterdir() if barcode_directory.is_dir()]
 
-            print(f"Running workflow on barcode directory: {barcode_directory.name}")
-            
+    for barcode_dir in subdirectories:
+        fastq_files = list(barcode_dir.glob("*.fastq.gz"))
+        for fastq_file in fastq_files:
+            print(f"Running workflow on barcode directory: {barcode_dir.name}")
+
             command = [
                 "geneious",
                 "-w", str(workflow_path),
+                "-i", str(fastq_file),
+                "-o", str(output_path / f"{barcode_dir.name}.csv")
             ]
-
-            for fastq_file in fastq_files:
-                command.extend(["-i", str(fastq_file)])
-                
-            command.extend(["-o", f"{output_path}/{barcode_directory.name}.csv",])
-
             subprocess.run(command, check=True)
 
 
@@ -36,8 +36,8 @@ def main():
     run_workflow(args.input_path, args.workflow_path, args.output_path)
 
     print(f"Input Path: {args.input_path}")
-    print(f"Workflow Path: {args.workflow}")
-    print(f"Output Path: {args.output}")
+    print(f"Workflow Path: {args.workflow_path}")
+    print(f"Output Path: {args.output_path}")
 
 if __name__ == '__main__':
     main()
